2e04c1c38e7b77a0e0410f66b47602e9a93f6dbd
Krishn Ramesh <krishnr@gmail.com>
2014-08-27 14:02:54 -0400

    New background and fixed background positioning

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
 $(document).ready(function() {
-    
-    $(window).load(function () {
-        resizeMain();
-    });
-    
-    $(window).resize(function () {
-        resizeMain();
-    });
-    
-    function resizeMain() {
-        var windowHeight = $(window).height();
-                
-        $("#main-container").css("height", windowHeight);
-        
-        var headerHeight = $(".header").height();
-        
-        $top = (windowHeight - headerHeight) / 2;
-        
-        $(".header").css("margin-top", $top);
-    }
-    
+
+
 });
